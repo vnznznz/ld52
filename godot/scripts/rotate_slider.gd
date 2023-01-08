0528a45ab6369c4d990a5efb16d8cc23781a3508
Vinzenz Sinapius <Vinzenz.Sinapius@gmail.com>
2023-01-08 14:11:13 +0100

    fps independet sliding

--- godot/scripts/rotate_slider.gd
+++ godot/scripts/rotate_slider.gd
@@ -23,15 +23,15 @@ func _process(delta):
 	$Arrow2Left.visible = false
 	if is_instance_valid(planet):		
 		if self.value <=40:
-			planet.rotate(-lerp(0.025, 0.05, (40-self.value) / 40))
+			planet.rotate(-lerp(0.5, 2, (40-self.value) / 40) * delta)
 			$Arrow2Left.visible = true
 		elif self.value <=170:
-			planet.rotate(-lerp(0, 0.025, (170-self.value) / 170))
+			planet.rotate(-lerp(0, 0.5, (170-self.value) / 170) * delta)
 			$Arrow1Left.visible = true
 		elif self.value >=320:
-			planet.rotate(lerp(0.025, 0.05, (self.value-320) / 40))
+			planet.rotate(lerp(0.5, 2, (self.value-320) / 40) * delta)
 			$Arrow2Right.visible = true
 		elif self.value >=190:
-			planet.rotate(lerp(0, 0.025, (self.value-190) / 190))
+			planet.rotate(lerp(0, 0.5, (self.value-190) / 190) * delta)
 			$Arrow1Right.visible = true
 		#planet.rotation_degrees = 180 - self.value
